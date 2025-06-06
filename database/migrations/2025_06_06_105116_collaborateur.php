c5b3d36f8255ad0f85dc4833a52d1b29b6c06e86
Georges-Lucas <georgeslucas.grondin@gmail.com>
2025-06-06 13:22:38 +0200

    ajout de la css sur l'acceuil et création de la base de donnée

---
+++ database/migrations/2025_06_06_105116_collaborateur.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collaborateurs', function (Blueprint $table) {
+            $table->id_collaborateur();
+            $table->string('nom');
+            $table->string('prenom'); 
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->date('date_naissance')->nullable();
+            $table->string('telephone')->nullable();
+            $table->string('photo')->nullable(); // URL or path to the photo
+            $table->string('adresse')->nullable();
+            $table->string('ville')->nullable();
+            $table->string('rôle');
+            $table->boolean('is_admin')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
